d0039f233ec8ca9c311d6070558237c9b2d5b60e
Bernat Borràs Civil <70479573+BernatBC@users.noreply.github.com>
2022-06-16 08:40:27 +0200

    Add files via upload

---
+++ 5-CameraTransformations_LoadOBJs/5-Exercise5/bloc-2/MyGLWidget.h
@@ -0,0 +1,33 @@
+// MyGLWidget.h
+#include "BL2GLWidget.h"
+#include "model.h"
+#define GLM_FORCE_RADIANS
+
+class MyGLWidget : public BL2GLWidget {
+  Q_OBJECT
+
+  public:
+    MyGLWidget(QWidget *parent=0) : BL2GLWidget(parent) {}
+    ~MyGLWidget();
+
+  protected:
+    virtual void carregaShaders ();
+    virtual void initializeGL ( );
+    virtual void paintGL ( );
+    virtual void keyPressEvent (QKeyEvent *event);
+    virtual void modelTransform ();
+
+    Model m;
+
+    GLuint projLoc, viewLoc, VAO_homer;
+
+    glm::vec3 OBS, VRP, up;
+    float FOV, ra, znear, zfar;
+    float rotation_angle = 0;
+
+  private:
+    int printOglError(const char file[], int line, const char func[]);
+    void projMatrixTransform();
+    void viewMatrixTransform();
+    void ini_camera();
+};
